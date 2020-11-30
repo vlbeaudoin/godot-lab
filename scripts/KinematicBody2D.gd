8689cf58aaef28e2be910a145f76c440424a93ef
Victor Lacasse-Beaudoin <vlbeaudoin@users.noreply.github.com>
2020-11-29 21:39:39 -0500

    Cleanup code and finish animations (idle/run)

--- scripts/KinematicBody2D.gd
+++ scripts/KinematicBody2D.gd
@@ -2,15 +2,17 @@ extends KinematicBody2D
 
 const DEBUG = true
 onready var debug_label = get_tree().get_nodes_in_group("debug_label")[0]
+var looking_left = false
 
 #onready var animated_sprite = $AnimatedSprite
+onready var sprite = $Sprite
+onready var animation_player = $Sprite/AnimationPlayer
 
 var msg_state
 
 enum {
 	IDLE,
-	LEFT,
-	RIGHT,
+	RUN,
 	ATTACK
 }
 
@@ -33,18 +35,13 @@ func enter_state(new_state):
 		match new_state:
 			IDLE:
 				state = IDLE
-#				animated_sprite.set_animation("idle")
-			LEFT:
-				if state != ATTACK:
-					state = LEFT
-#					animated_sprite.set_flip_h(true)
-			RIGHT:
-				if state != ATTACK:
-					state = RIGHT
-#					animated_sprite.set_flip_h(false)
+				animation_player.play("idle")
+			RUN:
+				state = RUN
+				animation_player.play("run")
 			ATTACK:
 				state = ATTACK
-#				animated_sprite.set_animation("attack")
+				animation_player.play("attack")
 				
 
 func get_input():
@@ -59,25 +56,29 @@ func get_input():
 	if Input.is_action_pressed("movement_down"):
 		input.y += 1
 	
-	if Input.is_action_pressed("attack"):
-		enter_state(ATTACK)
+	# Flip (look direction)
+	if input.x < 0:
+		looking_left = true
+	if input.x > 0:
+		looking_left = false
+	sprite.set_flip_h(looking_left)
+	
+#	if Input.is_action_pressed("attack"):
+#		enter_state(ATTACK)
 	
 	return input
 
 func process_movement():
 	# State
-	if input.x < 0:
-		enter_state(LEFT)
-	elif input.x > 0:
-		enter_state(RIGHT)
+	if input != Vector2.ZERO:
+		enter_state(RUN)
 	else:
 		enter_state(IDLE)
 	
 	# Direction-controlled movement
 	var direction = get_input()
 	if direction.length() > 0:
-		velocity = lerp(velocity, direction * attributes.speed, attributes.acceleration)
-#		velocity = lerp(velocity, direction.normalized() * attributes.speed, attributes.acceleration) # since we'll be using move_and_slide, no need to multiply by delta (or .normalize()) since the function already takes it into consideration.
+		velocity = lerp(velocity, direction * attributes.speed, attributes.acceleration)# since we'll be using move_and_slide, no need to multiply by delta (or .normalize()) since the function already takes it into consideration.
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, attributes.friction)
 	
@@ -87,36 +88,23 @@ func process_movement():
 	velocity = move_and_slide(velocity)
 
 func process_debug_label():
-#	match state:
-#				IDLE:
-#					msg_state = "Character is idle"
-#				LEFT:
-#					msg_state = "Character is looking left"
-#				RIGHT:
-#					msg_state = "Character is looking right"
-		
 	debug_label.set_debug_message(
 		"""Debug menu - F3
 		
 		Velocity: %s
-		Input: %s
-		State: %s
 		Attributes: %s
-		""" % [str(velocity), str(input), str(state), attributes]
+		State: %s
+		
+		Input: %s
+		looking_left: %s
+		""" % [str(velocity), attributes, str(state), str(input), looking_left]
 		)
 
 ## SETGET
-func get_attribute_skill():
-	return attributes.skill
-
 func set_attribute_acceleration(new_acceleration: float):
 	attributes.acceleration = new_acceleration
 
-
-## EXECUTION
-func _ready():
-	pass
-	
+## EXECUTION	
 func _physics_process(_delta):
 	process_debug_label()
 	process_movement()
